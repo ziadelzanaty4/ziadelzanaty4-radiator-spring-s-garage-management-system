d3dae58112ca668c9a5a8c56412396614f1a2637
ziadelzanaty4 <ziadelzanaty4@gmail.com>
2026-08-03 21:58:19 +0200

    added the menu of options

--- oop task.py
+++ oop task.py
@@ -16,4 +16,19 @@ class SupportVeichle(Car):
     def __init__(self, number, name, age, team, speed, capacity, CrewSize,ReliabilityRating):
         super().__init__(self, number, name, age, team, speed, capacity)
         self.CrewSize=CrewSize
-        self.ReliabilityRating=ReliabilityRating              
+        self.ReliabilityRating=ReliabilityRating
+
+
+
+Menu= f"""
+========Radiator Springs Garage========
+1. Check in a car
+2. View garage
+3. Tune-up
+4. Retire a car
+5. Find a car
+6. Garage report
+7. Exit
+=======================================
+"""
+print(Menu)
