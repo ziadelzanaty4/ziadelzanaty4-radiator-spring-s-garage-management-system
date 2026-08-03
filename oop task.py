0aee0b1a7cd2c807c0250660868f1f8814e288be
ziadelzanaty4 <ziadelzanaty4@gmail.com>
2026-08-03 21:30:33 +0200

    added class Car

---
+++ oop task.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, number, name, age, type, team, speed, capacity):
+        self.number=number
+        self.name=name
+        self.age=age
+        self.type=type
+        self.team=team
+        self.speed=speed
+        self.capacity=capacity
