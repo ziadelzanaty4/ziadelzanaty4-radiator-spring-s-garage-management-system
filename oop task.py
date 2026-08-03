1ba85776ee44d297c5d864370acb3bed49a9ac4f
ziadelzanaty4 <ziadelzanaty4@gmail.com>
2026-08-03 22:28:20 +0200

    added getters to Car class

--- oop task.py
+++ oop task.py
@@ -7,6 +7,20 @@ class Car:
         self._team=team
         self._speed=speed
         self._capacity=capacity
+    def get_number(self):
+        return self._number
+    def get_name(self):
+        return self._name
+    def get_age(self):
+        return self._age
+    def get_type(self):
+        return self._type
+    def get_team(self):
+        return self._team
+    def get_speed(self):
+        return self._speed
+    def get_capacity(self):
+        return self._capacity    
 
 class Racer(Car):
     def __init__(self, number, name, age, team, speed, capacity, races,laps):
